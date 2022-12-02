348d60d8493cb132d2f31999f84cccdde3a405bd
Lee Newton <lgnewton@gmail.com>
2022-12-02 11:08:05 +0000

    This code will live on forever!

--- idk.ps1
+++ idk.ps1
@@ -1,1 +1,19 @@
-Get-Command
+function Get-BathHairCoincidenceProbability {
+  param (
+    [Parameter(Mandatory = $true, Position = 0)]
+    [int32]$BathInterval,
+    [Parameter(Mandatory = $true, Position = 1)]
+    [int32]$HairInterval
+  )
+
+  $probabilityofcoincidence = $BathInterval * $HairInterval
+
+  Write-Host "The probability of today being a bath and hair day is 1 / $probabilityofcoincidence"
+
+  
+}  
+$bathint = Read-Host -Prompt "Please enter the number of days between baths "
+$hairint = Read-Host -Prompt "Please enter the number of days between hair cuts "
+  
+. Get-BathHairCoincidenceProbability $bathint $hairint
+## Just for reference this is not my code. I dont need to calculate this.
